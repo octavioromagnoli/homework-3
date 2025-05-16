6dc4040c87fd7d0b678f713c8d2a618bd4c09822
octavioromagnoli <octaromag@gmail.com>
2025-05-16 04:08:47 -0300

    Finished ejercicio 1 (format changes to main)

--- ej1/exes/main.cpp
+++ ej1/exes/main.cpp
@@ -2,9 +2,18 @@
 #include "Presion.hpp"
 #include "Posicion.hpp"
 #include "SaveFlightData.hpp"
+using namespace std;
 
 int main()
 {
+  cout << endl
+       << string(50, '=') << endl;
+  cout << "    EJERCICIO 1     " << endl;
+  cout << string(50, '=') << endl
+       << endl;
+
+  cout << "Se crearan los objetos Posicion y Presion" << endl
+       << endl;
   Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3f);
   Presion presion(101.3f, 5.8f, 6.1f);
 
@@ -13,42 +22,51 @@ int main()
 
   cout << endl;
 
-  // Crear un objeto SaveFlightData con posicion y presion como parámetros
+  cout << "Se serializarán dichos datos en el archivo creado (archivo-ej-1) utilizando la clase SaveFlightData " << endl;
+
   SaveFlightData saveFlightData(posicion, presion);
 
   // Serializar el objeto en un archivo "archivo-ej-1"
-  std::ofstream outFile("archivo-ej-1", std::ios::binary);
+  ofstream outFile("archivo-ej-1", ios::binary);
   if (outFile.is_open())
   {
     saveFlightData.serializar(outFile);
     outFile.close();
-    std::cout << "Datos serializados correctamente en 'archivo-ej-1'.\n";
+    cout << "Datos serializados correctamente en 'archivo-ej-1'.\n";
   }
   else
   {
-    std::cerr << "Error al abrir el archivo para serialización.\n";
+    cerr << "Error al abrir el archivo para serialización.\n";
   }
+
+  cout << endl
+       << "Se deserializarán dichos datos en un nuevo objeto de clase  SaveFlightData " << endl;
 
   Posicion posNula(0, 0, 0, 0);
   Presion presNula(0, 0, 0);
   // Crear otro objeto SaveFlightData y deserializar la información
   SaveFlightData loadedFlightData(posNula, presNula);
-  std::ifstream inFile("archivo-ej-1", std::ios::binary);
+  ifstream inFile("archivo-ej-1", ios::binary);
   if (inFile.is_open())
   {
     loadedFlightData.deserializar(inFile);
     inFile.close();
-    std::cout << "Datos deserializados correctamente desde 'archivo-ej-1'.\n";
+    cout << "Datos deserializados correctamente desde 'archivo-ej-1'.\n";
   }
   else
   {
-    std::cerr << "Error al abrir el archivo para deserialización.\n";
+    cerr << "Error al abrir el archivo para deserialización.\n";
   }
 
   cout << endl;
 
+  cout << "Resultado en objeto grabado mediante deserializacion: " << endl;
+
   // Imprimir los datos deserializados
   loadedFlightData.imprimir();
+
+  cout << endl
+       << "Se elimina el archivo \"archivo-ej-1\" (por prolijidad)" << endl;
 
   return 0;
 }
--- makefile
+++ makefile
@@ -2,4 +2,7 @@ SRCS := $(wildcard ej1/source/*.cpp) ej1/exes/main.cpp
 HEADERS := $(wildcard ej1/headers/*.h)
 
 run1: $(SRCS) $(HEADERS) ej1/exes/main.cpp
+	touch archivo-ej-1
 	g++ $(SRCS) -Wall -Wconversion -Wextra -Wpedantic -Iej1/headers -o ejercicio1exe
+	./ejercicio1exe
+	rm archivo-ej-1
